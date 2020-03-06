f40d131dd2f34cf083b47f11014d23b023f3cd8f
PJCollum <petercollum@hotmail.co.uk>
2020-03-06 17:32:17 +0000

    validation

--- index.js
+++ index.js
@@ -1,31 +1,60 @@
 const input = document.getElementById("input");
 const button = document.getElementById("submit");
 const deleteBtn = document.getElementById("delete");
+const deleteBtn1 = document.getElementById("delete1");
 
 let list = document.getElementsByTagName("ul")[0];
 
-submit.addEventListener("click", () =>{
+submit.addEventListener("click", () => {
     let listItem = document.createElement('li');
-    listItem.textContent = input.value;
-    console.log(listItem);
- 
-    list.appendChild(listItem);
-    input.value = '';
 
+    if (input.value == "") {
+        alert("Input must be filled out");
+        return false;
+    }
+
+    listItem.textContent = input.value;     //new input goes into list
+    console.log(listItem);
+
+
+//new li onclick delete 
+
+    list.appendChild(listItem);
+    input = input.value = "";       //clears input box after submit button clicked
+    return true
 })
 
-deleteBtn.addEventListener("click", () =>{
+deleteBtn.addEventListener("click", () => {
     let lastItem = document.querySelector('li:last-child');
     //let list = document.getElementsByTagName('ul')[0];
 
+    if (list.getElementsByTagName('li').length == 0) {                //if list array = 0, message ' nothing to delete'
+        alert("No items to delete")
+        return false;
+    }
     list.removeChild(lastItem);
+    return true;
 })
 
+//line delete button
 
-list.addEventListener('click', function(ev) {       //greys out and put line through
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
+// deleteBtnList.addEventListener("click", () => {
+//     let lastItem = document.querySelector('li:last-child');
+   
+
+//     if (list.getElementsByTagName('li').length == 0) {                //if list array = 0, message ' nothing to delete'
+//         alert("No items to delete")
+//         return false;
+//     }
+//     list.removeChild(lastItem);
+//     return true;
+// })
+
+
+list.addEventListener('click', function (ev) {       //greys out and put strike through line in
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
 }, false);
 
 
