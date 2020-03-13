f75becc3ea6320eb2620b2f9ce7df19b2f34e061
PJCollum <petercollum@hotmail.co.uk>
2020-03-13 09:27:01 +0000

    updated the X on each line

--- index.js
+++ index.js
@@ -5,14 +5,7 @@ const deleteBtn1 = document.getElementById("delete1");
 
 const date = new Date();
 
-
 let list = document.getElementsByTagName("ul")[0];
-
-
-function displayDate() {
-    document.getElementById("demo").innerHTML = date.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' })();
-    console.log(Date())
-}
 
 function checkTime(i) {
     if (i < 10) {
@@ -20,30 +13,63 @@ function checkTime(i) {
     }
     return i;
 }
+// function time() {
+// }
+
+
 submit.addEventListener("click", () => {
-
     let listItem = document.createElement('li');
-
-    function time() {
-        var date = new Date();
-        var hh = date.getHours();
-        var mm = date.getMinutes();
-        var ss = date.getSeconds();
-
-        // adding 0 for single digits
-
-        mm = checkTime(mm);
-        ss = checkTime(ss);
-
-        listItem.textContent = input.value + " " + "Created at " + hh + ":" + mm + ":" + ss;
-    }
 
     if (input.value == "") {
         alert("Input must be filled out");
         return false;
     }
 
-    time()
+    var date = new Date();
+    var hh = date.getHours();
+    var mm = date.getMinutes();
+    var ss = date.getSeconds();
+
+    listItem.textContent = input.value + " " + "Created at " + hh + ":" + mm + ":" + ss;
+
+
+    /*
+    2 text nodes
+
+    append first li
+
+    create span w/ class
+
+    append textnode2 to new span
+
+    appenmd span to listitem
+    */
+
+
+    // adding 0 for single digits
+    mm = checkTime(mm);
+    ss = checkTime(ss);
+
+    // // Create a "close" button and append it to each list item
+    // var myNodelist = document.getElementsByTagName("li");
+
+   
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode(" \u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        listItem.appendChild(span);
+    
+
+    // Click on a close button to hide the current list item
+    // var close = document.getElementsByClassName("close");
+    // var i;
+    // for (let i = 0; i < close.length; i++) {
+    span.onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+        // }
+        }
 
     console.log(listItem);
 
@@ -51,6 +77,7 @@ submit.addEventListener("click", () => {
     input = input.value = "";       //clears input box after submit button clicked
     return true
 })
+
 
 deleteBtn.addEventListener("click", () => {
     let lastItem = document.querySelector('li:last-child');
